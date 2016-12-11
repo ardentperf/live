850ffc5a17771e462751a4d42e6c85e6ab5beb65
Jeremy Schneider <jeremy.schneider@ardentperf.com>
2016-12-11 00:27:43 +0000

    add libdvd-pkg

--- customize.sh
+++ customize.sh
@@ -171,7 +171,8 @@ dpkg --add-architecture i386
 apt update
 
 # libxcb-xtest0 is prereq for zoom
-apt install -y gimp keepass2 keepassx printer-driver-hpijs secure-delete vlc xdotool google-chrome-stable git cubic syslinux-utils skype libxcb-xtest0
+# syslinux-utils needed to make bootable iso from cubic output
+apt install -y gimp keepass2 keepassx printer-driver-hpijs secure-delete vlc xdotool google-chrome-stable git cubic syslinux-utils skype libxcb-xtest0 libdvd-pkg
 
 cd /root
 wget https://zoom.us/client/latest/zoom_amd64.deb
